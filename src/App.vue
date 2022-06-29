a66ef069acdf9664af0da60a15510ac673776ee8
rubibyxd <69293474+rubibyxd@users.noreply.github.com>
2022-06-29 17:56:19 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <button>login</button>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<script>
+import './assets/sass/main.scss'
+export default{
+  name:'app',
+
+}
+</script>
